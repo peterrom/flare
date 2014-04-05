cc263c5fce18be62d8f789a0c6d07e9d33ba7e0f
Peter Román <peterrom@gmail.com>
2014-04-05 10:10:27 +0200

    Fix formatting of forty_print def/decl.

--- src/forty.c
+++ src/forty.c
@@ -1,7 +1,6 @@
 #include "forty.h"
 
-bool forty_print(const struct forty_action *actions,
-                 struct uio* s)
+bool forty_print(const struct forty_action *actions, struct uio *s)
 {
         return true;
 }
--- src/forty.h
+++ src/forty.h
@@ -16,7 +16,6 @@ struct forty_action {
         void (*pop) (void);
 };
 
-bool forty_print(const struct forty_action *actions,
-                 struct uio* s);
+bool forty_print(const struct forty_action *actions, struct uio *s);
 
 #endif /* FORTY_INCL */
