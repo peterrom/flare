073b053139d33fa8a6a98dce131e5ff798ef6386
Peter Román <peterrom@gmail.com>
2014-04-08 14:06:56 +0200

    Add documentation to SCRATCH.

--- src/scratch.h
+++ src/scratch.h
@@ -5,6 +5,27 @@
 #ifndef SCRATCH_INCL
 #define SCRATCH_INCL
 
+/* * Scratch space (SCRATCH)
+   SCRATCH is designed for processing data from a UIO stream in
+   chunks. It allows you to easily save parts of a chunk by setting
+   the ~scratch.valid_*~ pointers, i.e.
+
+   : struct scratch ss;
+   : scratch_init(&ss);
+
+   ... set the buffer to the string ~"01234"~ ...
+
+   : ss.valid_beg = scratch_find_any_of(&ss, "3");
+   : scratch_flush_left(&ss);
+
+   This will result in the buffer containing the string ~"34"~. You
+   can then fill the remaining chunk with
+
+   : scratch_fill_end(&ss, &is);
+
+   The buffer now contains ~{ '3', '4', '\0' }~ and then ~"567" ...~
+   or whatever is in the stream ~is~. */
+
 struct uio;
 
 struct scratch {
