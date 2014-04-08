3d1a05301eb8ab6f5c80dfd7345fb510210c1518
Peter Román <peterrom@gmail.com>
2014-04-08 11:54:20 +0200

    Add missing copyright notice to forty.c.

--- src/forty.c
+++ src/forty.c
@@ -1,3 +1,7 @@
+/* Copyright 2014 Peter Román.
+   This file is part of Flare which is licensed under GNU GPL v3.
+   See the file named LICENSE for details. */
+
 #include "forty.h"
 
 #include <string.h>
