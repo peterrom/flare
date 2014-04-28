a14e7eb8119cfbf68465c2cb628649254074c3aa
Peter Román <peterrom@gmail.com>
2014-04-28 19:49:42 +0200

    Add ATT, attribute macros.

---
+++ src/att.h
@@ -0,0 +1,10 @@
+/* Copyright 2014 Peter Román.
+   This file is part of Flare which is licensed under GNU GPL v3.
+   See the file named LICENSE for details. */
+
+#ifndef ATT_INCL
+#define ATT_INCL
+
+#define att_UNUSED __attribute__((unused))
+
+#endif /* ATT_INCL */
