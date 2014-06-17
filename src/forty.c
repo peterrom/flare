b650227c353149fbf0f88aeb59cb9292ae0ea4e1
Peter Román <peterrom@gmail.com>
2014-06-17 15:16:58 +0200

    FORTY: Remove the invalid_pushes nonsense.
    Using NULL tags is much simpler with no real drawbacks.

--- src/forty.c
+++ src/forty.c
@@ -11,73 +11,31 @@
 #include "scratch.h"
 #include "ss.h"
 
-/* ~ts~ is a tag stack. In addition to functioning as a "normal"
-   stack, it can keep track of "invalid" pushes/pops, i.e. pushes of
-   missing or ill-formated tags where you want the pop order to still
-   be correct. */
-
-struct ts_element {
-        const struct forty_tag *tp;
-        unsigned int invalid_pushes;
-};
-
-SS_DECLARE_STACK(ts, struct ts_element, 8)
-
-static void push_invalid(struct ss_ts *ts)
-{
-        if (ss_ts_empty(ts)) {
-                struct ts_element dummy = { NULL, 0 };
-                ss_ts_push(ts, dummy);
-        }
-
-        ++ss_ts_top(ts)->invalid_pushes;
-}
-
-static bool is_dummy(struct ts_element *e)
-{
-        return !e->tp;
-}
-
-static bool pop_invalid(struct ss_ts *ts)
-{
-        assert(!ss_ts_empty(ts));
-
-        struct ts_element *top = ss_ts_top(ts);
-
-        if (top->invalid_pushes == 0)
-                return false;
-
-        --top->invalid_pushes;
-
-        if (top->invalid_pushes == 0 && is_dummy(top))
-                ss_ts_pop(ts);
-
-        return true;
-}
+SS_DECLARE_STACK(ts, const struct forty_tag *, 8)
 
 static void push_tag(struct ss_ts *ts,
+                     int *alt_ts,
                      const struct forty_tag *tag,
                      void *context)
 {
-        struct ts_element e = {
-                .tp = tag,
-                .invalid_pushes = 0
-        };
-
-        if (tag && ss_ts_push(ts, e)) {
-                if (tag->push)
+        if (ss_ts_push(ts, tag)) {
+                if (tag && tag->push)
                         tag->push(context);
         } else {
-                push_invalid(ts);
+                ++(*alt_ts);
         }
 }
 
-static void pop_tag(struct ss_ts *ts, void *context)
+static void pop_tag(struct ss_ts *ts, int *alt_ts, void *context)
 {
-        if (ss_ts_empty(ts) || pop_invalid(ts))
-                return;
+        assert(!ss_ts_empty(ts));
 
-        const struct forty_tag *tag = ss_ts_pop(ts)->tp;
+        if (*alt_ts > 0) {
+                --(*alt_ts);
+                return;
+        }
+
+        const struct forty_tag *tag = *ss_ts_pop(ts);
 
         if (tag && tag->pop)
                 tag->pop(context);
@@ -85,11 +43,12 @@ static void pop_tag(struct ss_ts *ts, void *context)
 
 static void pop_remaining(struct ss_ts *ts, void *context)
 {
-        struct ts_element *e;
+        while (!ss_ts_empty(ts)) {
+                const struct forty_tag *e = *ss_ts_pop(ts);
 
-        while ((e = ss_ts_pop(ts)))
-                if (e->tp && e->tp->pop)
-                        e->tp->pop(context);
+                if (e && e->pop)
+                        e->pop(context);
+        }
 }
 
 static const struct forty_tag *tl_find(const struct forty_tag *tl,
@@ -131,11 +90,12 @@ static char *handle_text(struct scratch *s,
         return end;
 }
 
-static char *handle_pops(struct scratch *s, void *context, struct ss_ts *ts)
+static char *handle_pops(struct scratch *s, void *context,
+                         struct ss_ts *ts, int *alt_ts)
 {
         char *end = s->valid_beg;
         for (; end != s->valid_end && *end == '}'; ++end)
-                pop_tag(ts, context);
+                pop_tag(ts, alt_ts, context);
 
         return end;
 }
@@ -150,7 +110,7 @@ static char *tag_end_or_end(char *beg, char *end)
 
 static char *handle_tag(struct scratch *s, void *context,
                         const struct forty_tag *tl,
-                        struct ss_ts *ts)
+                        struct ss_ts *ts, int *alt_ts)
 {
         char *const beg = s->valid_beg + 1;
         char *const tag_end = tag_end_or_end(beg, s->valid_end);
@@ -161,12 +121,12 @@ static char *handle_tag(struct scratch *s, void *context,
         const struct forty_tag *tag =
                 tl_find(tl, beg, (size_t) (tag_end - beg));
 
-        push_tag(ts, tag, context);
+        push_tag(ts, alt_ts, tag, context);
 
         return tag_end + 1;
 }
 
-static bool parse_scratch(struct scratch *s, struct ss_ts *ts,
+static bool parse_scratch(struct scratch *s, struct ss_ts *ts, int *alt_ts,
                           const struct forty_tag *tl,
                           void *context,
                           void print(void *, struct ui *))
@@ -176,10 +136,10 @@ static bool parse_scratch(struct scratch *s, struct ss_ts *ts,
 
                 switch (*s->valid_beg) {
                 case '{':
-                        end = handle_tag(s, context, tl, ts);
+                        end = handle_tag(s, context, tl, ts, alt_ts);
                         break;
                 case '}':
-                        end = handle_pops(s, context, ts);
+                        end = handle_pops(s, context, ts, alt_ts);
                         break;
                 default:
                         end = handle_text(s, context, print);
@@ -195,11 +155,12 @@ static bool parse_scratch(struct scratch *s, struct ss_ts *ts,
         return true;
 }
 
-static void reset_state(struct ui *is, struct scratch *s, struct ss_ts *ts)
+static void reset_state(struct ui *is, struct scratch *s,
+                        struct ss_ts *ts, int *alt_ts)
 {
         ui_find(is, " ", 1);
         scratch_clear(s);
-        push_invalid(ts);
+        push_tag(ts, alt_ts, NULL, NULL);
 }
 
 void forty_parse(const struct forty_tag *tl,
@@ -213,10 +174,12 @@ void forty_parse(const struct forty_tag *tl,
         struct ss_ts ts;
         ss_ts_init(&ts);
 
+        int alt_ts = 0;
+
         while (scratch_fill(&s, is)) {
-                if (!parse_scratch(&s, &ts, tl, context, print) &&
+                if (!parse_scratch(&s, &ts, &alt_ts, tl, context, print) &&
                     scratch_full(&s))
-                        reset_state(is, &s, &ts);
+                        reset_state(is, &s, &ts, &alt_ts);
         }
 
         pop_remaining(&ts, context);
