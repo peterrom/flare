c63b49374441891745c682da04f7d645389df0a7
Peter Román <peterrom@gmail.com>
2014-08-11 07:56:34 +0200

    GLENN: Add simple test.

--- src/testglenn.c
+++ src/testglenn.c
@@ -4,9 +4,28 @@
 
 #include "tf.h"
 #include "glenn.h"
+#include "chum.h"
 
 tf_TEST(init)
 {
+        struct chum_context ctx;
+        tf_ASSERT(chum_init(&ctx));
+
+        tf_IN_CASE_OF_ASSERT(
+                chum_terminate(&ctx);
+                );
+
+        int width;
+        int height;
+        chum_window_size(&ctx, &width, &height);
+
+        struct glenn_state s;
+        glenn_init(&s, width, height);
+
+        glenn_display(&s);
+        tf_ASSERT(chum_refresh(&ctx));
+
+        chum_terminate(&ctx);
 }
 
 tf_SUITE(glenn)
