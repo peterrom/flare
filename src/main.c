8de92b670d45643c6c508707995b81de7197ed7d
Peter Román <peterrom@gmail.com>
2016-10-04 13:37:15 +0200

    MAIN: Use SEC triangle

--- src/main.c
+++ src/main.c
@@ -1,24 +1,29 @@
-/* Copyright 2014 Peter Román.
+/* Copyright 2014, 2016 Peter Román.
    This file is part of Flare which is licensed under GNU GPL v3.
    See the file named LICENSE for details. */
 
 #include "chum.h"
 #include "glenn.h"
+#include "sec.h"
 
 int main(void)
 {
+        struct sec_world w;
+        sec_world_init(&w);
+
+        const int i = sec_new(&w);
+        w.component[i] = sec_c_triangle;
+        w.triangle[i] = (struct sec_triangle) {
+                .vx = {{  0.0f,  0.5f,   0.0f, 1.0f },
+                       {  0.5f, -0.366f, 0.0f, 1.0f },
+                       { -0.5f, -0.366f, 0.0f, 1.0f }}};
+
         struct chum_context ctx;
         chum_init(&ctx);
 
-        const float vs[] = {
-                 0.0f,    0.5f,   0.0f, 1.0f,
-                 0.5f,   -0.366f, 0.0f, 1.0f,
-                -0.5f,   -0.366f, 0.0f, 1.0f
-        };
-
         struct glenn_state s;
         glenn_init(&s, 3);
-        glenn_update(&s, vs);
+        glenn_update(&s, (const float *) w.triangle[i].vx);
         glenn_resize(&s, 640, 480);
 
         while (chum_refresh(&ctx)) {
